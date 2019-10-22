103652310ad24fc902a0aa58f92cc8641dc6b366
Johnathanseymour696 <56037937+Johnathanseymour696@users.noreply.github.com>
2019-10-22 09:04:59 -0700

    Add files via upload

--- hMan.py
+++ hMan.py
@@ -2,30 +2,11 @@ import random
 import os 
 import sys
 import time
-print("Welcome to hangman")
-print("For easy game press e")
-print("For hard game press h")
-print("To quit press q")
-menu = input("What would you like to do?")
-
-if menu == "e":
-	List = [
-     "cat","pony","zebra","kangaroo"
-	]
-	mysteryWord = random.choice(List)
-	guess_word = []
-	print(mysteryWord)
-	for letter in mysteryWord:
-		guess_word.append("_")
-	print(guess_word)
-	framelist = ['''
-	|===
-    |  |
-	   |
-	   |
-	   |
-	  ===
-	''']
-
-	guess = input('what letter would you like to put?')
-	
+EwordList = [
+"lion","cat","mouse","bat"
+         ]
+guess_word = []
+cguess = random.choice(EwordList)#The computer picks the random options
+lengthw = len(cguess)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+letterS = []
